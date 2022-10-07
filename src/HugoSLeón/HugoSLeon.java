459c220956c3e83eb9832e2eaf40fd8e160fe373
LAPTOP-96V6OUN6\Hugo <lhugo4468@gmail.com>
2022-10-07 11:42:15 -0500

    no message

--- src/HugoSLeón/HugoSLeon.java
+++ src/HugoSLeón/HugoSLeon.java
@@ -2,12 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package roberthJRiascos;
+package HugoSLeón;
 
 /**
  *
- * @author PERSONAL
+ * @author Hugo
  */
-public class RoberthJRiascos {
-    
+public class HugoSLeon {
+    public String hola; 
+    public HugoSLeon() {
+    }
 }
