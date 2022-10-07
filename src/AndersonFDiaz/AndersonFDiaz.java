eba390e62d6fcfd5178788b7200d86f9a44076cf
DESKTOP-KQBQRFR\Anderson Diaz <adiazciro@gmail.com>
2022-10-07 08:34:59 -0500

    Clase AndersonFDiaz

---
+++ src/AndersonFDiaz/AndersonFDiaz.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AndersonFDiaz;
+
+/**
+ *
+ * @author Anderson Diaz
+ */
+public class AndersonFDiaz {
+        public String saludo; 
+    public AndersonFDiaz() {
+    }
+}
